af419d58ca4cbe7c7d8b7eb3dd170db3d1841da4
Henrik Lievonen <henrik.lievonen@hotmail.com>
2021-09-13 10:22:17 +0300

    Make pio! macro take size of the program as input

--- tests/proc.rs
+++ tests/proc.rs
@@ -1,6 +1,7 @@
 #[test]
 fn test_pio_proc() {
     let p = pio_proc::pio!(
+        1,
         "
     label:
         jmp label
@@ -20,6 +21,7 @@ fn test_pio_proc() {
 #[test]
 fn test_pio_proc2() {
     let p = pio_proc::pio!(
+        32,
         "
     .origin 5
     public label:
@@ -41,4 +43,17 @@ fn test_pio_proc2() {
     );
     assert_eq!(p.public_defines.label, 0);
     assert_eq!(p.public_defines.owo, 2);
+}
+
+#[test]
+fn test_pio_proc_size() {
+    // Inline constant size
+    pio_proc::pio!(32, "label:\njmp label\n");
+    // Constant variable
+    const PROGRAM_SIZE: usize = 32;
+    pio_proc::pio!(PROGRAM_SIZE, "label:\njmp label\n");
+    // Expression
+    pio_proc::pio!(10 + 20, "label:\njmp label\n");
+    // Constant from another crate
+    pio_proc::pio!(pio::RP2040_MAX_PROGRAM_SIZE, "label:\njmp label\n");
 }
