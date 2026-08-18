fbe7dfb452c8b87df49fc2abac43cf2e02e5ad0e
Grzegorz Nosek <root@localdomain.pl>
2026-08-18 07:32:18 +0200

    Replace ArrayVec in Program with a custom Array
    This makes it possible to construct a Program at compile time.

--- pio-core/src/lib.rs
+++ pio-core/src/lib.rs
@@ -624,7 +624,7 @@ impl<const PROGRAM_SIZE: usize> Assembler<PROGRAM_SIZE> {
         };
 
         Program {
-            code,
+            code: Array::from_slice(code.as_slice()),
             origin: None,
             side_set,
             wrap,
@@ -863,11 +863,51 @@ pub struct Wrap {
     pub target: u8,
 }
 
+/// Compile-time constant array for storing the program code
+///
+/// The program can be shorter than the maximum length (N). The array dereferences
+/// to a slice of the proper length (equal to the length of the original slice).
+#[derive(Debug)]
+pub struct Array<T: Copy, const N: usize> {
+    data: [T; N],
+    len: usize,
+}
+
+impl<T: Copy, const N: usize> Array<T, N> {
+    /// Create a new array from a slice
+    ///
+    /// Panics if data.len() > N.
+    pub const fn from_slice(data: &[T]) -> Self {
+        assert!(data.len() <= N, "Slice longer than maximum array size");
+        let mut array = [data[0]; N];
+        let (array_data, _) = array.split_at_mut(data.len());
+        array_data.copy_from_slice(data);
+        Self {
+            data: array,
+            len: data.len(),
+        }
+    }
+
+    /// Get the actual length of the stored data
+    ///
+    /// May be smaller than N.
+    pub const fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<T: Copy, const N: usize> core::ops::Deref for Array<T, N> {
+    type Target = [T];
+    fn deref(&self) -> &Self::Target {
+        &self.data[..self.len]
+    }
+}
+
 /// Program ready to be executed by PIO hardware.
 #[derive(Debug)]
 pub struct Program<const PROGRAM_SIZE: usize> {
     /// Assembled program code.
-    pub code: ArrayVec<u16, PROGRAM_SIZE>,
+    pub code: Array<u16, PROGRAM_SIZE>,
     /// Offset at which the program must be loaded.
     ///
     /// Most often 0 if defined. This might be needed when using data based `JMP`s.
